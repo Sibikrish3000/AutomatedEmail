1dcc48fa2b8e0b5a786054cfd36f79b85b668605
Sibi Krishnamoorthy <sibikrish3000@gmail.com>
2024-11-15 05:02:37 +0530

    worker 2

--- worker.js
+++ worker.js
@@ -239,7 +239,9 @@ app.post('/send-email', verifyBearerToken, (req, res) => {
   });
 
 });
-
+const PORT = process.env.PORT || 3000;
+console.log(`Server is running on port worker `);
+app.listen();
 // const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
