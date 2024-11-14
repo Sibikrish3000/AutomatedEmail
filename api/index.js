e3a6198de7da34d2e54991dccb532aa2e6f35055
Sibi Krishnamoorthy <sibikrish3000@gmail.com>
2024-11-15 03:37:48 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -239,8 +239,4 @@ app.post('/send-email', verifyBearerToken, (req, res) => {
   });
 
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+);
